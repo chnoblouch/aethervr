78195d7236087b4f2f02b037fd7a4c384837c009
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2025-12-22 17:37:08 +0100

    tracker: detect gestures more reliably
    the landmark distance threshold for detecting a gesture is now based on the distance of the hand from the camera, which makes gesture detection more robust when the hand is close to the camera or far away

--- tracker/aethervr/gesture_detector.py
+++ tracker/aethervr/gesture_detector.py
@@ -40,15 +40,19 @@ class GestureDetector:
 
         if not tracking_state.visible:
             return
+        
+        p0 = tracking_state.landmarks[0]
+        p1 = tracking_state.landmarks[9]
+        reference_distance = GestureDetector.calc_distance(p0, p1)
 
-        if GestureDetector.is_fist(tracking_state):
+        if GestureDetector.is_fist(tracking_state, reference_distance):
             tracking_state.gesture = Gesture.FIST
-        elif GestureDetector.is_pinching(tracking_state):
+        elif GestureDetector.is_pinching(tracking_state, reference_distance):
             if GestureDetector.is_front_facing(tracking_state.landmarks, flipped):
                 tracking_state.gesture = Gesture.PINCH
             else:
                 tracking_state.gesture = Gesture.PALM_PINCH
-        elif GestureDetector.is_middle_pinching(tracking_state):
+        elif GestureDetector.is_middle_pinching(tracking_state, reference_distance):
             tracking_state.gesture = Gesture.MIDDLE_PINCH
 
             if config.thumbstick_enabled:
@@ -80,21 +84,25 @@ class GestureDetector:
             input_state.jostick_center = None
 
     @staticmethod
-    def is_pinching(tracking_state: HandState):
+    def is_pinching(tracking_state: HandState, reference_distance: float):
         tip1 = tracking_state.landmarks[4]
         tip2 = tracking_state.landmarks[8]
-        max_distance = 0.1 if tracking_state.previous_gesture == Gesture.PINCH else 0.06
-        return GestureDetector.calc_distance(tip1, tip2) < max_distance
+        distance = GestureDetector.calc_distance(tip1, tip2) / reference_distance
+
+        distance_threshold = 0.5 if tracking_state.previous_gesture == Gesture.PINCH else 0.3
+        return distance < distance_threshold
 
     @staticmethod
-    def is_middle_pinching(tracking_state: HandState):
+    def is_middle_pinching(tracking_state: HandState, reference_distance: float):
         tip1 = tracking_state.landmarks[4]
         tip2 = tracking_state.landmarks[12]
-        max_distance = 0.1 if tracking_state.previous_gesture == Gesture.MIDDLE_PINCH else 0.06
-        return GestureDetector.calc_distance(tip1, tip2) < max_distance
+        distance = GestureDetector.calc_distance(tip1, tip2) / reference_distance
+
+        distance_threshold = 0.5 if tracking_state.previous_gesture == Gesture.MIDDLE_PINCH else 0.3
+        return distance < distance_threshold
 
     @staticmethod
-    def is_fist(tracking_state: HandState):
+    def is_fist(tracking_state: HandState, reference_distance: float):
         tip0 = tracking_state.landmarks[4]
         tip1 = tracking_state.landmarks[8]
         tip2 = tracking_state.landmarks[12]
@@ -102,13 +110,13 @@ class GestureDetector:
         tip4 = tracking_state.landmarks[20]
         wrist = tracking_state.landmarks[0]
 
-        return (
-            GestureDetector.calc_distance(tip0, wrist) < 0.25
-            and GestureDetector.calc_distance(tip1, wrist) < 0.25
-            and GestureDetector.calc_distance(tip2, wrist) < 0.15
-            and GestureDetector.calc_distance(tip3, wrist) < 0.25
-            and GestureDetector.calc_distance(tip4, wrist) < 0.25
-        )
+        d0 = GestureDetector.calc_distance(tip0, wrist) / reference_distance
+        d1 = GestureDetector.calc_distance(tip1, wrist) / reference_distance
+        d2 = GestureDetector.calc_distance(tip2, wrist) / reference_distance
+        d3 = GestureDetector.calc_distance(tip3, wrist) / reference_distance
+        d4 = GestureDetector.calc_distance(tip4, wrist) / reference_distance
+
+        return (d0 < 1.4 and d1 < 1.4 and d2 < 1.0 and d3 < 1.4 and d4 < 1.4)
 
     @staticmethod
     def calc_distance(landmark_a, landmark_b):
